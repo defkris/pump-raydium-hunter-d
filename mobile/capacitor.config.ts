7bfea5af1b8cf69ba3510ebb52c80164099d48e8
defkris <krzys133777@gmail.com>
2025-08-23 13:03:25 +0000

    Delete mobile/capacitor.config.ts

--- mobile/capacitor.config.ts
+++
@@ -1,2 +0,0 @@
-import { defineConfig } from '@capacitor/cli'
-export default defineConfig({ appId:'com.yourname.prh', appName:'Pump Raydium Hunter', webDir:'../frontend/dist', server:{ androidScheme:'https' } })
